a4ccbe57f5a968c9713755882b2e654a6de665f1
bdbaraban <375@holbertonschool.com>
2018-12-04 23:41:18 +0000

    Checking for possible insertion at front

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -20,13 +20,20 @@ listint_t *insert_node(listint_t **head, int number)
 
 	if (head == NULL || *head == NULL)
 		return (NULL);
-
 	node = *head;
+
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
-
 	new->n = number;
+
+	if (node->n >= number)
+	{
+		new->next = node;
+		*head = new;
+		return (new);
+	}
+
 	while (node && node->next && node->next->n < number)
 		node = node->next;
 
