fe9596c1cd7cf23bd45dbe24b40781e7cc2f5082
bdbaraban <375@holbertonschool.com>
2018-12-04 22:20:48 +0000

    Printing negative sign for negative numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,6 +2,8 @@
 import random
 number = random.randint(-10000, 10000)
 digit = abs(number) % 10
+if number < 0:
+    digit = -digit
 print("Last digit of {} is {} and is ".format(number, digit), end="")
 if digit > 5:
     print("greater than 5")
