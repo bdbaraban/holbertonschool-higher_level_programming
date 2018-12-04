f300a783f37ec13524229269aad4cea6279e23f8
bdbaraban <375@holbertonschool.com>
2018-12-04 23:07:13 +0000

    Removing extra blank line

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,7 +2,6 @@
 # 3-print_alphabt.py
 # Brennan D Baraban <375@holbertonschool.com>
 
-
 """Print the alphabet in lowercase except for the letters q and e."""
 for letter in range(97, 123):
     if chr(letter) is not 'q' and chr(letter) is not 'e':
