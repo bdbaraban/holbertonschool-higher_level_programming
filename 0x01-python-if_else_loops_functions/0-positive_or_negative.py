0af10655fa2cebce41a87cb150e96511c0473e91
bdbaraban <375@holbertonschool.com>
2018-12-04 22:14:53 +0000

    Fixing typos in output

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,8 +2,8 @@
 import random
 number = random.randint(-10, 10)
 if number > 0:
-    print("is positive")
+    print("{} is positive".format(number))
 elif number == 0:
-    print("is zero")
+    print("{} is zero".format(number))
 else:
-    print("is negative")
+    print("{} is negative".format(number))
--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,6 +6,6 @@ print("Last digit of {} is {} and is ".format(number, digit), end="")
 if digit > 5:
     print("greater than 5")
 elif digit == 0:
-    print("and is 0")
+    print("0")
 else:
     print("less than 6 and not 0")
