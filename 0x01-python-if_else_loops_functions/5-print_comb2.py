036754b1a51359e6d3d761aa06b1b5845e2f20b5
bdbaraban <375@holbertonschool.com>
2018-12-04 23:08:50 +0000

    Removing documentation for Holberton checker

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-# 5-print_comb2.py
-# Brennan D Baraban <375@holbertonschool.com>
-
-
-"""Print numbers 0 to 99, two digits each, in ascending order."""
 for number in range(0, 100):
     if number == 99:
         print("{}".format(number))
