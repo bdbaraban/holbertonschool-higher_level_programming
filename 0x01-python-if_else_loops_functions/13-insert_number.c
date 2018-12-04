14f2f3b95600b8799e1edf140e7c313bc7994629
bdbaraban <375@holbertonschool.com>
2018-12-04 23:35:55 +0000

    Adding null checking for head and next node

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -18,7 +18,7 @@ listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *node, *new, *tmp;
 
-	if (*head == NULL)
+	if (head == NULL || *head == NULL)
 		return (NULL);
 
 	node = *head;
@@ -27,7 +27,7 @@ listint_t *insert_node(listint_t **head, int number)
 		return (NULL);
 
 	new->n = number;
-	while (node && node->next->n < number)
+	while (node && node->next && node->next->n < number)
 		node = node->next;
 
 	tmp = node->next;
