3c5581ea44850890fdb209c499dd18fabbe150c9
bdbaraban <375@holbertonschool.com>
2018-12-04 22:15:21 +0000

    Implementing format into print call

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for letter in range(97, 123):
-    print(chr(letter), end="")
+    print("{}".format(chr(letter)), end="")
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for letter in range(97, 123):
     if chr(letter) is not 'q' and chr(letter) is not 'e':
-        print(chr(letter), end="")
+        print("{}".format(chr(letter)), end="")
