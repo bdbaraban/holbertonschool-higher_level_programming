f97144a8e32dca90dd8986f21eff8fd4970f0720
bdbaraban <375@holbertonschool.com>
2018-12-05 22:30:28 +0000

    Adding elif to account for one arg

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,6 +9,8 @@ if __name__ == "__main__":
     count = len(sys.argv) - 1
     if count == 0:
         print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
     else:
         print("{} arguments:".format(count))
     for i in range(count):
