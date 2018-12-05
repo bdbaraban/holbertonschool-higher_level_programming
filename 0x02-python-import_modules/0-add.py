555f921996ac6a545cdf335b2c64c69d16c452e0
bdbaraban <375@holbertonschool.com>
2018-12-05 20:45:51 +0000

    Add import-checking

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,8 +4,9 @@
 
 """Print the result of the addition 1 + 2 = 3."""
 
-from add_0 import add
+if __name__ == "__main__":
+    from add_0 import add
 
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add(a, b)))
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
