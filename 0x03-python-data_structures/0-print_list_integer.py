fb11069a9aa5a2acf1af22af9e1bb844673df202
bdbaraban <375@holbertonschool.com>
2018-12-11 20:28:07 +0000

    Removing documentation for checker

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-# 0-print_list_integer.py
-# Brennan D Baraban <375@holbertonschool.com>
-
-
 def print_list_integer(my_list=[]):
     """Print all integers of a list."""
     for i in range(len(my_list)):
