9635ec73501e2e0bc7976633c8d52d8ff2861126
bdbaraban <375@holbertonschool.com>
2018-12-11 22:29:28 +0000

    Using backwards range instead of .reverse

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -5,6 +5,5 @@
 
 def print_reversed_list_integer(my_list=[]):
     """Print all integers of a list in reverse order."""
-    my_list.reverse()
-    for i in range(len(my_list)):
+    for i in range(len(my_list) - 1, -1, -1):
         print("{:d}".format(my_list[i]))
