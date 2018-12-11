d17d659fa4a417a357eebeb434aa4e655bc58656
bdbaraban <375@holbertonschool.com>
2018-12-11 22:31:59 +0000

    Compacting for loop syntax

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -5,5 +5,6 @@
 
 def print_reversed_list_integer(my_list=[]):
     """Print all integers of a list in reverse order."""
-    for i in range(len(my_list) - 1, -1, -1):
-        print("{:d}".format(my_list[i]))
+    my_list.reverse()
+    for i in my_list:
+        print("{:d}".format(i))
