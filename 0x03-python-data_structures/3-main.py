9bafa2739cb53d53afa81167d1e65bed116f17ec
bdbaraban <375@holbertonschool.com>
2018-12-11 22:40:39 +0000

    Checking if list is empty before printing

--- 0x03-python-data_structures/3-main.py
+++ 0x03-python-data_structures/3-main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
 
-my_list = []
+my_list = [1, 2, 3]
 print_reversed_list_integer(my_list)
