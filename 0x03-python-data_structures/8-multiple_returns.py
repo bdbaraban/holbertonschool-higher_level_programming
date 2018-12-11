8a2eedc9b50aa39851a7abce67256b25434811ed
bdbaraban <375@holbertonschool.com>
2018-12-11 22:56:22 +0000

    Returning None for empty strings

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -5,4 +5,6 @@
 
 def multiple_returns(sentence):
     """Returns the length of a string and its first character."""
+    if sentence == "":
+        return (0, None)
     return (len(sentence), sentence[0])
