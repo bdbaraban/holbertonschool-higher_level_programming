4640fe93702ab434e0a486a3fdd9f582406e2ce0
bdbaraban <375@holbertonschool.com>
2018-12-11 22:49:29 +0000

    Checking argument type before printing

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -5,7 +5,8 @@
 
 def new_in_list(my_list, idx, element):
     """Replace an element in a copied list at a specific position."""
-    if idx >= 0 and idx < len(my_list):
-        copy = [x for x in my_list]
-        copy[idx] = element
-    return (copy)
+    if isinstance(my_list, list):
+        if idx >= 0 and idx < len(my_list):
+            copy = [x for x in my_list]
+            copy[idx] = element
+        return (copy)
