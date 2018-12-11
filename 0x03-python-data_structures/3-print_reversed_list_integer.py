26127c53b71b9dcaa986215f8f7ce7eb31583f9d
bdbaraban <375@holbertonschool.com>
2018-12-11 22:45:41 +0000

    Checking argument type before printing

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -5,6 +5,7 @@
 
 def print_reversed_list_integer(my_list=[]):
     """Print all integers of a list in reverse order."""
-    my_list.reverse()
-    for i in my_list:
-        print("{:d}".format(i))
+    if isinstance(my_list, list):
+        my_list.reverse()
+        for i in my_list:
+            print("{:d}".format(i))
