6d95558ccefa5c2d9f7363582e3e20c2db0eb269
bdbaraban <375@holbertonschool.com>
2018-12-12 00:37:14 +0000

    Recursively checking from middle onward

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -5,33 +5,34 @@
 
 #include "lists.h"
 
-int recursive_is_palindrome(listint_t *h, size_t size);
+int recursive_is_palindrome(listint_t *head, listint_t *check, size_t idx);
 int is_palindrome(listint_t **head);
 
 /**
  * recursive_is_palindrome - Recursively checks if a singly-linked
  *                           list is a palindrome.
- * @h: The head of the singly-linked list.
- * @size: The size of the singly linked list.
+ * @head: The head of the singly-linked list.
+ * @check: The next node of the list to compare.
+ * @idx: The indexed node from head to compare to check.
  *
  * Return: If the linked list is not a palindrome - 0.
  *         If the linked list is a palindrome - 1.
  */
-int recursive_is_palindrome(listint_t *h, size_t size)
+int recursive_is_palindrome(listint_t *head, listint_t *check, size_t idx)
 {
-	listint_t *tmp = h;
+	listint_t *tmp = head;
 	size_t i;
 
-	for (i = 0; i < size; i++)
-		tmp = tmp->next;
-
-	if (h == tmp || h->next == tmp)
+	if (idx == 0)
 		return (1);
 
-	if (h->n != tmp->n)
+	for (i = 0; i < idx; i++)
+		tmp = tmp->next;
+
+	if (tmp->n != check->n)
 		return (0);
 
-	return (recursive_is_palindrome(h->next, size - 2));
+	return (recursive_is_palindrome(head, check->next, idx - 1));
 }
 
 /**
@@ -44,17 +45,31 @@ int recursive_is_palindrome(listint_t *h, size_t size)
 int is_palindrome(listint_t **head)
 {
 	listint_t *tmp;
-	size_t size = 0;
+	size_t size = 0, i;
 
 	if (*head == NULL)
 		return (1);
 
 	tmp = *head;
-	while (tmp->next)
+	while (tmp)
 	{
 		size++;
 		tmp = tmp->next;
 	}
 
-	return (recursive_is_palindrome(*head, size));
+	tmp = *head;
+	for (i = 0; i < (size / 2) - 1; i++)
+		tmp = tmp->next;
+
+	if ((size % 2) == 0 && tmp->n != tmp->next->n)
+		return (0);
+
+	if ((size % 2) != 0)
+		size = (size / 2) - 1;
+	else
+		size = (size / 2) - 2;
+
+	tmp = tmp->next->next;
+
+	return (recursive_is_palindrome(*head, tmp, size));
 }
