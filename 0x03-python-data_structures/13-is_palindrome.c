4c76e5a35ebbf7fcbfc7aeb81b863ceabcecdc3d
bdbaraban <375@holbertonschool.com>
2018-12-12 01:40:59 +0000

    Checking for single-noded linked lists

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -42,7 +42,7 @@ int is_palindrome(listint_t **head)
 	listint_t *tmp, *rev, *mid;
 	size_t size = 0, i;
 
-	if (*head == NULL)
+	if (*head == NULL || (*head)->next == NULL)
 		return (1);
 
 	tmp = *head;
