efc354090f2b7e9179660564728341f83b609a68
bdbaraban <375@holbertonschool.com>
2018-12-12 05:36:14 +0000

    Completed bonus task 14

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,30 @@
+/*
+ * File: 100-print_python_list_info.c
+ * Auth: Brennan D Baraban
+ */
+
+#include <Python.h>
+
+/**
+ * print_python_list_info - Prints basic info about Python lists.
+ * @p: A PyObject list.
+ */
+void print_python_list_info(PyObject *p)
+{
+	int size, alloc, i;
+	PyObject *obj;
+
+	size = Py_SIZE(p);
+	alloc = ((PyListObject *)p)->allocated;
+
+	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %d\n", alloc);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Element %d: ", i);
+
+		obj = PyList_GetItem(p, i);
+		printf("%s\n", Py_TYPE(obj)->tp_name);
+	}
+}
