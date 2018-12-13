9509a4494a6b398fc908adc00f85f10deec17097
bdbaraban <375@holbertonschool.com>
2018-12-13 23:59:12 +0000

    Reassigning big value and using .items method

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -8,9 +8,10 @@ def best_score(a_dictionary):
     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
 
-    big = list(a_dictionary.values())[0]
     ret = list(a_dictionary.keys())[0]
-    for key in a_dictionary:
-        if a_dictionary[key] > big:
-            ret = key
-    return (key)
+    big = a_dictionary[ret]
+    for k, v in a_dictionary.items():
+        if v > big:
+            big = v
+            ret = k
+    return (ret)
