ea4ec96cf780ca0f8b9e86f83100e161b71b174e
bdbaraban <375@holbertonschool.com>
2018-12-14 01:52:28 +0000

    Compacting map call

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return (list(map(lambda x: (list(map(lambda y: matrix[x][y] ** 2, range(len(matrix[x]))))), range(len(matrix[0])))))
+    return list(map(lambda x: list(map(lambda y: y**2, x[:])), matrix))
