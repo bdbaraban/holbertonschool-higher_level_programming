44b0b1cd1aef98c680a0a70c61ff5c01825ec1c6
bdbaraban <375@holbertonschool.com>
2018-12-14 02:00:48 +0000

    Checking for empty lists

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -5,6 +5,9 @@
 
 def weight_average(my_list=[]):
     """Return the weighted average of all integers in a list of tuples."""
+    if not isinstance(my_list, list) or len(my_list) == 0:
+        return (0)
+
     avg = 0
     size = 0
     for tup in my_list:
