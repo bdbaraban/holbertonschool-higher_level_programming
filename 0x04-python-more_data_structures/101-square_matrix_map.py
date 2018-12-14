4debb803af5d6a1f10db8c96b79e1c5d820401a3
bdbaraban <375@holbertonschool.com>
2018-12-14 01:29:18 +0000

    Refactoring without for loop

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return ([list(map(lambda x: x * x, row)) for row in matrix])
+    return (list(map(lambda x: (list(map(lambda y: matrix[x][y] ** 2, range(len(matrix[x]))))), range(len(matrix[0])))))
