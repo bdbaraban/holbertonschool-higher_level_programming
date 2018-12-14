6ff220933c30ed1bfc23581232356807ce4dc3ef
bdbaraban <375@holbertonschool.com>
2018-12-14 00:07:34 +0000

    Checking for non-Roman numeral characters'

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -19,9 +19,13 @@ def roman_to_int(roman_string):
     }
     num = 0
     for i in range(len(roman_string)):
+        if roman_dict.get(roman_string[i], 0) == 0:
+            return None
+
         if (i != (len(roman_string) - 1) and
                 roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
                 num += roman_dict[roman_string[i]] * -1
+
         else:
             num += roman_dict[roman_string[i]]
     return (num)
