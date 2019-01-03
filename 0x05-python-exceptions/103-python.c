49e65a75d2deb3366fbdf68a222320c84e9b6a13
bdbaraban <375@holbertonschool.com>
2019-01-03 00:25:00 +0000

    Implement calls to fflush before each printf

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -24,21 +24,25 @@ void print_python_list(PyObject *p)
 	alloc = list->allocated;
 
 	fflush(stdout);
-
 	printf("[*] Python list info\n");
 	if (strcmp(p->ob_type->tp_name, "list") != 0)
 	{
+		fflush(stdout);
 		printf("  [ERROR] Invalid List Object\n");
 		return;
 	}
 
+	fflush(stdout);
 	printf("[*] Size of the Python List = %d\n", size);
+	fflush(stdout);
 	printf("[*] Allocated = %d\n", alloc);
 
 	for (i = 0; i < size; i++)
 	{
 		type = list->ob_item[i]->ob_type->tp_name;
+		fflush(stdout);
 		printf("Element %d: %s\n", i, type);
+		fflush(stdout);
 		if (strcmp(type, "bytes") == 0)
 			print_python_bytes(list->ob_item[i]);
 		else if (strcmp(type, "float") == 0)
@@ -56,15 +60,17 @@ void print_python_bytes(PyObject *p)
 	PyBytesObject *bytes = (PyBytesObject *)p;
 
 	fflush(stdout);
-
 	printf("[.] bytes object info\n");
 	if (strcmp(p->ob_type->tp_name, "bytes") != 0)
 	{
+		fflush(stdout);
 		printf("  [ERROR] Invalid Bytes Object\n");
 		return;
 	}
 
+	fflush(stdout);
 	printf("  size: %ld\n", ((PyVarObject *)p)->ob_size);
+	fflush(stdout);
 	printf("  trying string: %s\n", bytes->ob_sval);
 
 	if (((PyVarObject *)p)->ob_size > 10)
@@ -72,10 +78,13 @@ void print_python_bytes(PyObject *p)
 	else
 		size = ((PyVarObject *)p)->ob_size + 1;
 
+	fflush(stdout);
 	printf("  first %d bytes: ", size);
 	for (i = 0; i < size; i++)
 	{
+		fflush(stdout);
 		printf("%02hhx", bytes->ob_sval[i]);
+		fflush(stdout);
 		if (i == (size - 1))
 			printf("\n");
 		else
@@ -94,18 +103,23 @@ void print_python_float(PyObject *p)
 	PyFloatObject *float_obj = (PyFloatObject *)p;
 
 	fflush(stdout);
-
 	printf("[.] float object info\n");
 	if (strcmp(p->ob_type->tp_name, "float") != 0)
 	{
+		fflush(stdout);
 		printf("  [ERROR] Invalid Float Object\n");
 		return;
 	}
 
+	fflush(stdout);
 	printf("  value: %.15g", float_obj->ob_fval);
 	round = (int)float_obj->ob_fval;
 	if (float_obj->ob_fval - round == 0 ||
 	    round - float_obj->ob_fval == 0)
+	{
+		fflush(stdout);
 		printf(".0");
+	}
+	fflush(stdout);
 	printf("\n");
 }
