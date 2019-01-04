9b4dbc1acbd564c3a65b65a52f181cdda0407edd
bdbaraban <375@holbertonschool.com>
2019-01-04 00:47:00 +0000

    Fix my_print to only print position when it is 0

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -74,7 +74,8 @@ class Square:
     def my_print(self):
         """Print the square with the # character."""
         for i in range(0, self.__size):
-            [print(" ", end="") for i in range(0, self.__position[0])]
+            if (self.__position[1] == 0):
+                [print(" ", end="") for i in range(0, self.__position[0])]
             [print("#", end="") for j in range(0, self.__size)]
             print("")
         if self.__size == 0:
