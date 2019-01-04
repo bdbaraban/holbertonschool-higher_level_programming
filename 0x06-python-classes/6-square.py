d21cadb1065da42d15a06508f1b5e34020ce21c8
bdbaraban <375@holbertonschool.com>
2019-01-04 03:06:04 +0000

    Fix my_print to check for empty size first

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -73,10 +73,12 @@ class Square:
 
     def my_print(self):
         """Print the square with the # character."""
+        if self.__size == 0:
+            print("")
+            return
+
         [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
-            print("")
-        if self.__size == 0:
             print("")
