1c46388441351f8d15a887952520abb0b23889e6
bdbaraban <375@holbertonschool.com>
2019-01-04 06:28:18 +0000

    Fill out docstrings to exactly match line count of bytecode

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -4,14 +4,18 @@ import math
 
 class MagicClass:
     def __init__(self, radius):
-        """Initialize a magic class."""
+        """Initialize a magic class.
+        Arg: radius (float or int): The radius of the new MagicClass.
+        """
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
         self.__radius = radius
 
     def area(self):
+        """Return the area of the MagicClass."""
         return (self.__radius ** 2 * math.pi)
 
     def circumference(self):
+        """Return The circumference of the MagicClass."""
         return (2 * math.pi * self.__radius)
