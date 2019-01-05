eba76f663e1179feb1c8c38d701db42bb9da7387
bdbaraban <375@holbertonschool.com>
2019-01-05 04:49:08 +0000

    Specify exception error to catch and update documentation

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,13 +4,18 @@
 
 
 def safe_print_list(my_list=[], x=0):
-    """Print x elememts of a list."""
+    """Print x elememts of a list.
+
+    Args:
+        my_list (list): The list to print elements from.
+        x (int): The number of elements of my_list to print.
+    """
     ret = 0
-    for i in range(0, x):
+    for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
             ret += 1
-        except:
+        except IndexError:
             break
     print("")
     return (ret)
