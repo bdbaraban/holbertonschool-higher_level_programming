f0859f0bd9ac7edd570a9bc2ab73865057318c30
bdbaraban <375@holbertonschool.com>
2019-01-09 19:23:57 +0000

    Fix printing for sizes of 0 and add test for no arguments provided

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -18,9 +18,6 @@ def print_square(size):
     if size < 0:
         raise ValueError("size must be >= 0")
 
-    if size == 0:
+    for i in range(size):
+        [print("#", end="") for j in range(size)]
         print("")
-    else:
-        for i in range(size):
-            [print("#", end="") for j in range(size)]
-            print("")
