500a111f7486391a682a049cc3af50e459c1fda0
bdbaraban <375@holbertonschool.com>
2019-01-18 22:39:32 +0000

    Compact code with ternary if conditional

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -4,9 +4,6 @@
 
 """"Print the alphabet in reverse order alternating upper- and lower-case."""
 i = 0
-for c in range(122, 96, -1):
+for c in range(ord('z'), ord('a') - 1, -1):
     print("{}".format(chr(c - i)), end="")
-    if i == 0:
-        i = 32
-    else:
-        i = 0
+    i = 32 if i == 0 else 0
