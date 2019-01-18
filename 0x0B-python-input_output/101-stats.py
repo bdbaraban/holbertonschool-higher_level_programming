01beae62651d8cb19b22445d65844af6829b1d7f
bdbaraban <375@holbertonschool.com>
2019-01-18 05:42:38 +0000

    Upload completed task 101

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# 101-stats.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Reads from standard input and computes metrics.
+
+After every ten lines or the input of a keyboard interruption (CTRL + C),
+prints the following statistics:
+    - Total file size up to that point.
+    - Count of read status codes up to that point.
+"""
+
+if __name__ == "__main__":
+    size = 0
+    status_codes = {}
+
+    while True:
+        for i in range(10):
+            line = input()
+            line = line.split()
+
+            size += int(line[8])
+            if status_codes.get(line[7], -1) == -1:
+                status_codes[line[7]] = 1
+            else:
+                status_codes[line[7]] += 1
+
+        print("File size: {}".format(size))
+        for key in sorted(status_codes):
+            print("{}: {}".format(key, status_codes[key]))
