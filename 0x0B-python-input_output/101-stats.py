329a8520b28787f90235326cdd1a632e72e80d38
bdbaraban <375@holbertonschool.com>
2019-01-19 04:35:32 +0000

    Implement error checking for invalid line format

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -26,23 +26,33 @@ if __name__ == "__main__":
 
     size = 0
     status_codes = {}
+    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    count = 0
 
     try:
-        count = 0
         for line in sys.stdin:
-            line = line.split()
-
-            size += int(line[8])
-            if status_codes.get(line[7], -1) == -1:
-                status_codes[line[7]] = 1
-            else:
-                status_codes[line[7]] += 1
-
-            if count == 9:
+            if count == 10:
                 print_stats(size, status_codes)
                 count = 0
             else:
                 count += 1
+
+            line = line.split()
+
+            try:
+                size += int(line[-1])
+            except (IndexError, ValueError):
+                pass
+
+            try:
+                if line[-2] in valid_codes:
+                    if status_codes.get(line[-2], -1) == -1:
+                        status_codes[line[-2]] = 1
+                    else:
+                        status_codes[line[-2]] += 1
+            except IndexError:
+                pass
+
         print_stats(size, status_codes)
 
     except KeyboardInterrupt:
