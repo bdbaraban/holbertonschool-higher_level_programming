681c7e7adb2c86c9425167c033e4e43c50ec45c0
bdbaraban <375@holbertonschool.com>
2019-03-05 23:56:17 +0000

    Specify database privileges for user 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- Creates the database hbtn_0d_2 and the user user_0d_2
--- The user_0d_2 has SELECT privilege with password user_0d_2_pwd
+-- The user_0d_2 has SELECT privilege on hbtn_0d_2 with password user_0d_2_pwd
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON *.* TO 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 FLUSH PRIVILEGES;
