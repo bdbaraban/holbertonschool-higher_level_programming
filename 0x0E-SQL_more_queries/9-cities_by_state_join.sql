fc2e42df4b4f494a1514110d42eb4777e1f226f2
bdbaraban <375@holbertonschool.com>
2019-03-06 03:39:17 +0000

    Capitalize as statement

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -2,6 +2,6 @@
 -- Records are sorted in order of ascending cities.id.
 SELECT c.`id`, c.`name`, s.`name`
   FROM `cities` AS c
-       INNER JOIN `states` as s
+       INNER JOIN `states` AS s
        ON c.`state_id` = s.`id`
  ORDER BY c.`id`;
