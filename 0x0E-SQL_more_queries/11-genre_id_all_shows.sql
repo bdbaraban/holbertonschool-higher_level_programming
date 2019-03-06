ddd05081ee69de3e6fd769dacfe5f160c23b3b6d
bdbaraban <375@holbertonschool.com>
2019-03-06 04:21:21 +0000

    Capitalize as statements

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -2,7 +2,7 @@
 -- Displays NULL for shows without genres.
 -- Records are ordered by ascending tv_shows.title and tv_show_genres.genre_id.
 SELECT s.`title`, g.`genre_id`
-  FROM `tv_shows` as s
-       LEFT JOIN `tv_show_genres` as g
+  FROM `tv_shows` AS s
+       LEFT JOIN `tv_show_genres` AS g
        ON s.`id` = g.`show_id`
  ORDER BY s.`title`, g.`genre_id`;
