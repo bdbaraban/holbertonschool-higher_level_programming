960891e58fba5380621ca09464605b6a6e156e2d
bdbaraban <375@holbertonschool.com>
2019-03-29 20:16:34 +0000

    Make SQL search case sensitive

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,9 +11,7 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    c.execute("SELECT * FROM `states` WHERE `name` = '{}'".format(sys.argv[4]))
-    states = c.fetchall()
-    if len(states) != 0:
-        [print(state) for state in states]
-    else:
-        print("")
+    c.execute("SELECT * \
+                 FROM `states` \
+                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
+    [print(state) for state in c.fetchall()]
