be9b493a80c2cb6fc1d8e22f6b2ad798c71d8fb5
bdbaraban <375@holbertonschool.com>
2019-03-29 21:18:17 +0000

    Fix attribute types in class docstring

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -12,9 +12,9 @@ class City(Base):
     """Represents a city for a MySQL database.
 
     Attributes:
-        id (sqlalchemy.Column): The city's id.
-        name (sqlalchemy.Column): The city's name.
-        state_id (sqlalchemy.Column): The city's state id.
+        id (str): The city's id.
+        name (sqlalchemy.Integer): The city's name.
+        state_id (sqlalchemy.String): The city's state id.
     """
     __tablename__ = "cities"
     id = Column(Integer, primary_key=True)
