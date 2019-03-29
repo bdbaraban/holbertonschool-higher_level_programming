cf3a696da413b51c2ca3e5f097bc2440d13ab044
bdbaraban <375@holbertonschool.com>
2019-03-29 20:39:29 +0000

    Create SQLAlchemy within script

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -8,13 +8,13 @@ import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from relationship_state import State
-from relationship_city import City
-
+from relationship_city import Base, City
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
