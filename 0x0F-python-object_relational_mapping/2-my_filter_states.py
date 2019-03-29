0284c66279a6c9656d59887a2398d1930a738ddc
bdbaraban <375@holbertonschool.com>
2019-03-29 19:55:38 +0000

    Order SQL query by states.id

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,5 +11,8 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    c.execute("SELECT * FROM `states` WHERE `name` = '{}'".format(sys.argv[4]))
+    c.execute("SELECT * FROM `states` \
+                WHERE `name` = '{}' \
+                ORDER BY `id`"
+              .format(sys.argv[4]))
     [print(state) for state in c.fetchall()]
