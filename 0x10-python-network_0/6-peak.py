068e02a9e1f69005719352fc146e270e49d9c5ad
bdbaraban <375@holbertonschool.com>
2019-04-10 21:30:43 +0000

    Upload task 6 - Python peak-finding algorithm with time complexity

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Defines a peak-finding algorithm."""
+
+
+def find_peak(list_of_integers):
+    """Return a peak in a list of unsorted integers."""
+    if list_of_integers == []:
+        return None
+
+    size = len(list_of_integers)
+    if size == 1:
+        return list_of_integers[0]
+    elif size == 2:
+        return max(list_of_integers)
+
+    mid = int(size / 2)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid + 1:])
