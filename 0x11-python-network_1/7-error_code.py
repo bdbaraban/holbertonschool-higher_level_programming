3662702e9adcfbb345de88d6a2f805bf779d96df
bdbaraban <375@holbertonschool.com>
2019-04-15 20:21:13 +0000

    Upload task 7 - script that sends a request to a given URL, handling HTTP errors, using the requests module

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Sends a request to a given URL and displays the response body.
+
+Usage: ./7-error_code.py <URL>
+  - Handles HTTP errors.
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
