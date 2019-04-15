375a46e98c01fd0b31b1f5a3f7a033e8693004ac
bdbaraban <375@holbertonschool.com>
2019-04-15 22:20:32 +0000

    Swap formatting in URL building - insert second argument (owner) before first (repo name)

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,7 +9,7 @@ import requests
 
 if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits".format(
-        sys.argv[1], sys.argv[2])
+        sys.argv[2], sys.argv[1])
 
     r = requests.get(url)
     commits = r.json()
