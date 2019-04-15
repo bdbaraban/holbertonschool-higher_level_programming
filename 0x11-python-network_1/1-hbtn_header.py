ece981cb5420c179b2354c8fb106a1927d9db3d1
bdbaraban <375@holbertonschool.com>
2019-04-15 20:14:31 +0000

    Upload task 1 - script to get X-Request-Id header var of request to given URL

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Displays the X-Request-Id header variable of a request to a given URL.
+
+Usage: ./1-hbtn_header <URL>
+"""
+import sys
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
