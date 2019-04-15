4df51a1955317d3d841523848e464f21bfd211c0
bdbaraban <375@holbertonschool.com>
2019-04-15 20:16:31 +0000

    Specify .py extension in usage documentation of module docstring

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Displays the X-Request-Id header variable of a request to a given URL.
 
-Usage: ./1-hbtn_header <URL>
+Usage: ./1-hbtn_header.py <URL>
 """
 import sys
 import urllib.request
