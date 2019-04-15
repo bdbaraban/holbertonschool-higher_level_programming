9d963a00eebd277c9a5cd16c737b97fe6456a908
bdbaraban <375@holbertonschool.com>
2019-04-15 20:11:41 +0000

    Import HTTPBasicAuth so as to get ID through GitHub API basic authentication

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,8 +6,10 @@ Usage: ./10-my_github.py <GitHub username> <GitHub password>
 """
 import sys
 import requests
+from requests.auth import HTTPBasicAuth
 
 
 if __name__ == "__main__":
-    r = requests.get("https://api.github.com/users/{}".format(sys.argv[1]))
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
     print(r.json().get("id"))
