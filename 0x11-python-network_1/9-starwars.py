b0723891d46b15b97c8b447711c3e6f208cfd278
bdbaraban <375@holbertonschool.com>
2019-04-15 20:22:20 +0000

    Upload task 9 - script that searches the Star Wars API with a given string

---
+++ 0x11-python-network_1/9-starwars.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Sends a search request for a given string to the Star Wars API.
+
+Usage: ./9-starwars.py <search string>
+  - The search request is sent to the Star Wars API search people endpoint.
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://swapi.co/api/people/?search={}".format(sys.argv[1])
+
+    r = requests.get(url)
+    results = r.json()
+    print("Number of results: {}".format(results.get("count")))
+    [print(r.get("name")) for r in results.get("results")]
