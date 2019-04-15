9e4f6b2eb09450004ba2a443e48af72a4e937c0b
bdbaraban <375@holbertonschool.com>
2019-04-15 23:41:38 +0000

    Upload task 102 - update to task 101, printing films for each character

---
+++ 0x11-python-network_1/102-starwars.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Sends a search request for a given string to the Star Wars API.
+
+For each character matched, displays the associated list of movies.
+Mangages pagination to display all results.
+
+Usage: ./102-starwars.py <search string>
+  - The search request is sent to the Star Wars API search people endpoint.
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://swapi.co/api/people/?search={}".format(sys.argv[1])
+    results = requests.get(url).json()
+
+    count = results.get("count")
+    print("Number of results: {}".format(results.get("count")))
+
+    c = 0
+    while c < count:
+        for r in results.get("results"):
+            print(r.get("name"))
+            for link in r.get("films"):
+                film = requests.get(link).json()
+                print("\t{}".format(film.get("title")))
+            c += 1
+
+        next_page = results.get("next")
+        if next_page is not None:
+            results = requests.get(next_page).json()
