2dcf4f8ad49c6533807f94e5886d45f57cf93a43
bdbaraban <375@holbertonschool.com>
2019-04-15 20:24:38 +0000

    Output fix: Use \t tab escape character instead of manually entering spaces

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen(request) as response:
         body = response.read()
         print("Body response:")
-        print("    - type: {}".format(type(body)))
-        print("    - content: {}".format(body))
-        print("    - utf8 content: {}".format(body.decode("utf-8")))
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
