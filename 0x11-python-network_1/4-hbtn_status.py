868cf2a94178b548ec2186c15d475923822a6fe7
bdbaraban <375@holbertonschool.com>
2019-04-15 20:25:27 +0000

    Output fix: Use \t tab escape character instead of manually entering spaces

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,5 +6,5 @@ import requests
 if __name__ == "__main__":
     r = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print("    - type: {}".format(type(r.text)))
-    print("    - content: {}".format(r.text))
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
