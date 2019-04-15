d523b3ddd91b10bfbf93962ff65e7f0a5275b8ac
bdbaraban <375@holbertonschool.com>
2019-04-15 22:27:52 +0000

    Error handling: Run commit printing loop in try-except to handle cases where repo has less than 10 commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -13,7 +13,10 @@ if __name__ == "__main__":
 
     r = requests.get(url)
     commits = r.json()
-    for i in range(10):
-        print("{}: {}".format(
-            commits[i].get("sha"),
-            commits[i].get("commit").get("author").get("name")))
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
