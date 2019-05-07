2503157503b835c19adb39d178c6a49e65c36839
bdbaraban <375@holbertonschool.com>
2019-05-07 21:14:11 +0000

    Fix third string - expected 'Javascript is amazing'

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-for (const line of ['C is fun', 'Python is cool', 'Javascript is awesome']) {
+for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
   console.log(line);
 }
