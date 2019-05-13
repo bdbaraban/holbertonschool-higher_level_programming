bbd3805a82150791ecd8ea0ddb2df276f62e7455
bdbaraban <375@holbertonschool.com>
2019-05-13 20:07:20 +0000

    Upload task 3: Rectangle class with print method

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
+  }
+};
