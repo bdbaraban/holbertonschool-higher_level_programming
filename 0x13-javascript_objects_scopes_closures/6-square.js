657e2dad9aacf957d4782a05b174541b465ee24f
bdbaraban <375@holbertonschool.com>
2019-05-13 20:15:03 +0000

    Upload task 6: Square class extending Rectangle with printChar method

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+module.exports = class Square extends require('./5-square.js') {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
+    }
+  }
+};
