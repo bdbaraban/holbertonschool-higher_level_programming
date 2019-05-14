2f5f252d4a1b5c866f5b7b885efbc490a5332f2d
bdbaraban <375@holbertonschool.com>
2019-05-14 20:43:40 +0000

    Upload task 100: Printing character names for given movie on Star Wars API

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const request = require('request');
+const url = 'https://swapi.co/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    characters.forEach((character) => {
+      request(character, function (error, response, body) {
+        if (!error) {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    });
+  }
+});
