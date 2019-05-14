5e4ff24bd76ec52856bc005a258af6909304ba4b
bdbaraban <375@holbertonschool.com>
2019-05-14 20:21:29 +0000

    Use find method on each movie within reduce to improve efficiency

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,7 +4,7 @@ request(process.argv[2], function (error, response, body) {
   if (!error) {
     const results = JSON.parse(body).results;
     console.log(results.reduce((count, movie) => {
-      return movie.characters.includes('https://swapi.co/api/people/18/', 0)
+      return movie.characters.find((character) => character.endsWith('/18/'))
         ? count + 1
         : count;
     }, 0));
