029f8ef30aecb2fc8fa5a148489d83ceb971fe7d
bdbaraban <375@holbertonschool.com>
2019-05-14 19:58:20 +0000

    Ignore error instead of printing it

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
-request('http://swapi.co/api/films', function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
     const results = JSON.parse(body).results;
     console.log(results.reduce((count, movie) => {
       return movie.characters.includes('https://swapi.co/api/people/18/', 0)
